6f27add8d3e12e009723453bc07cfc4f8f075c28
dan1768 <dan1768@6dd3a268-fd34-0410-9aca-e39d7cf66fee>
2007-07-16 20:10:34 +0000

    Function looping added

--- trunk/VirtualEarth/GeolabVirtualMaps/js/lcd_demo/Functions.js
+++ trunk/VirtualEarth/GeolabVirtualMaps/js/lcd_demo/Functions.js
@@ -1,14 +1,19 @@
-﻿var haynnispin;
+﻿var shape;
 var haynnislatlon = new VELatLong(41.65588797740388,-70.28833866119386);
 function AddPin(lat, long, title, description)
 {
     var pinlatlong = new VELatLong(lat, long);
+    shape = new VEShape(VEShapeType.Pushpin, pinlatlong);  
+    shape.SetCustomIcon('/images/bus_stop_icon.png');
+    shape.SetTitle(title);         
+    shape.SetDescription(description); 
+    map.AddShape(shape);
     //haynnispin = map.AddPushpin(haynnislatlon) ; 
     //haynnispin.SetTitle('Haynnis'); 
     //haynnispin.SetDescription('Haynnis Transportation center');
-    haynnispin = map.AddPushpin(pinlatlong) ; 
-    haynnispin.SetTitle(title); 
-    haynnispin.SetDescription(description);
+    //haynnispin = map.AddPushpin(pinlatlong) ; 
+    //haynnispin.SetTitle(title); 
+    //haynnispin.SetDescription(description);
 }
 function step1(){
     var t = 5000; // loading time
@@ -18,10 +23,9 @@ function step1(){
     setTimeout("map.SetMapStyle('a')", 5000);
     setTimeout("map.SetMapStyle('h')", 10000);
     setTimeout("map.SetMapStyle('r')", 15000);
-    setTimeout("map.ShowInfoBox(haynnispin)",16000);
-    setTimeout("map.HideInfoBox(haynnispin)",21000);
+    setTimeout("map.ShowInfoBox(shape)",16000);
+    setTimeout("map.HideInfoBox(shape);map.DeleteShape(shape);",21000);
     setTimeout("step2()",23000);
-    
 }
 function step2(){
     map.PanToLatLong(new VELatLong(42.05842543936369,-70.19500602493133));
@@ -31,8 +35,11 @@ function step2(){
     setTimeout("map.ZoomIn()",7500);
 
     setTimeout("map.AddShape(polyline['Orange']);map.SetMapView(polyline['Orange'].GetPoints());map.ShowInfoBox(polyline['Orange'])",11000);
+    setTimeout("map.HideInfoBox(polyline['Orange']);map.DeleteShape(polyline['Orange'])",16000);
     setTimeout("map.AddShape(polyline['PTown']);map.SetMapView(polyline['PTown'].GetPoints());",17000);
-    setTimeout("map.DeleteShape(polyline['Orange'])",16000);
-    setTimeout("map.HideInfoBox(polyline['Orange']);map.DeleteShape(polyline['PTown'])",23000);
+    setTimeout("map.DeleteShape(polyline['PTown'])",23000);
+    setTimeout("map.Clear();map.DeleteAllShapes();",24000);
+    setTimeout("setRoutes()",24500);
+    setTimeout("step1()",25000);
 }
 
