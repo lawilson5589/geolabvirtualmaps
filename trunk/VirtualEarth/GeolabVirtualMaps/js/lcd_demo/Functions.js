90384af6e52b353bed8f7e3ea03408a1a7533c55
dan1768 <dan1768@6dd3a268-fd34-0410-9aca-e39d7cf66fee>
2007-07-17 19:17:29 +0000

    Added Timings.

--- trunk/VirtualEarth/GeolabVirtualMaps/js/lcd_demo/Functions.js
+++ trunk/VirtualEarth/GeolabVirtualMaps/js/lcd_demo/Functions.js
@@ -1,4 +1,9 @@
-﻿var shape;
+﻿//Main Animation Sequence File- AVL Display
+//Authored by J.Baltikauskas, D.Fitch 2007
+//GeoGraphics Lab
+//Bridgewater State College
+
+var shape;
 var haynnislatlon = new VELatLong(41.65588797740388,-70.28833866119386);
 function AddPin(lat, long, title, description)
 {
@@ -18,7 +23,7 @@ function AddPin(lat, long, title, description)
 function step1(){
     var t = 5000; // loading time
     map.SetMapStyle("r");
-    AddPin( 41.65588797740388, -70.28833866119386, "Haynnis", "Big Test");
+    AddPin( 41.65588797740388, -70.28833866119386, "Hyannis", "Hyannis Transportation Center");
     map.SetCenterAndZoom(haynnislatlon, 9);
     setTimeout("map.SetMapStyle('a')", 5000);
     setTimeout("map.SetMapStyle('h')", 10000);
@@ -33,13 +38,110 @@ function step2(){
     setTimeout("map.ZoomIn()",4500);
     setTimeout("map.ZoomIn()",6000);
     setTimeout("map.ZoomIn()",7500);
-
-    setTimeout("map.AddShape(polyline['Orange']);map.SetMapView(polyline['Orange'].GetPoints());map.ShowInfoBox(polyline['Orange'])",11000);
-    setTimeout("map.HideInfoBox(polyline['Orange']);map.DeleteShape(polyline['Orange'])",16000);
+    // Pin for orange 42.051775,-70.18581
+    setTimeout("map.AddShape(polyline['Orange']);map.SetMapView(polyline['Orange'].GetPoints());",11000);
+    setTimeout("map.ShowInfoBox(polyline['Orange']);",12000);
+    setTimeout("map.HideInfoBox(polyline['Orange']);map.DeleteShape(polyline['Orange']);",16000);
     setTimeout("map.AddShape(polyline['PTown']);map.SetMapView(polyline['PTown'].GetPoints());",17000);
-    setTimeout("map.DeleteShape(polyline['PTown'])",23000);
-    setTimeout("map.Clear();map.DeleteAllShapes();",24000);
-    setTimeout("setRoutes()",24500);
-    setTimeout("step1()",25000);
+    setTimeout("map.ShowInfoBox(polyline['PTown']);",18000);
+    setTimeout("map.DeleteShape(polyline['PTown']);",23000);
+    setTimeout("step3()",24000);
+    
 }
+function step3(){
 
+    //Flex Route Procedure
+    map.ZoomOut();
+    setTimeout("map.ZoomOut();map.PanToLatLong(new VELatLong(41.867337,-70.078368));",3000);
+    setTimeout("map.ZoomOut();", 6000);
+    setTimeout("map.AddShape(polyline['Flex']);map.SetMapView(polyline['Flex'].GetPoints());",7500);
+    setTimeout("map.ShowInfoBox(polyline['Flex']);",8500);
+    setTimeout("map.HideInfoBox(polyline['Flex']);map.DeleteShape(polyline['Flex']);",13500);
+    
+    //H20 Route Procedure
+    setTimeout("map.ZoomIn();map.PanToLatLong(new VELatLong(41.722110,-70.118581));",14500);
+    setTimeout("map.AddShape(polyline['H20']);map.SetMapView(polyline['H20'].GetPoints());",15500);
+    setTimeout("map.ShowInfoBox(polyline['H20']);",16500);
+    setTimeout("map.HideInfoBox(polyline['H20']);map.DeleteShape(polyline['H20']);",21500);
+    setTimeout("step4()",22500);
+      
+}
+function step4(){
+
+    //Yarmouth Procedure
+    map.ZoomIn();
+    setTimeout("map.PanToLatLong(new VELatLong(41.651322,-70.233572));",3000);
+    setTimeout("map.ZoomIn();",4000);
+    setTimeout("map.AddShape(polyline['Yarmouth']);map.SetMapView(polyline['Yarmouth'].GetPoints());",6000);
+    setTimeout("map.ShowInfoBox(polyline['Yarmouth']);",7000);
+    setTimeout("map.HideInfoBox(polyline['Yarmouth']);map.DeleteShape(polyline['Yarmouth']);",12000);
+    
+    //Woosh Procedure
+    setTimeout("map.PanToLatLong(new VELatLong(41.6,-70.4));",12500);
+    setTimeout("map.PanToLatLong(new VELatLong(41.541994,-70.636582));",13000);
+    setTimeout("map.AddShape(polyline['Whoosh']);map.SetMapView(polyline['Whoosh'].GetPoints());",16000);
+    setTimeout("map.ShowInfoBox(polyline['Whoosh']);",17000);
+    setTimeout("map.HideInfoBox(polyline['Whoosh']);map.DeleteShape(polyline['Whoosh']);",22000);
+    setTimeout("step5()",23000);
+    
+}
+function step5(){
+
+    //Sealine Procedure
+    map.ZoomOut();
+    setTimeout("map.PanToLatLong(new VELatLong(41.613246,-70.439368));",3000);
+    setTimeout("map.AddShape(polyline['Sealine']);map.SetMapView(polyline['Sealine'].GetPoints());",5000);
+    setTimeout("map.ShowInfoBox(polyline['Sealine']);",6000);
+    setTimeout("map.HideInfoBox(polyline['Sealine']);map.DeleteShape(polyline['Sealine']);",11000);
+    
+    //Barnstable Procedure
+    setTimeout("map.PanToLatLong(new VELatLong(41.680348,-70.311813));",12000);
+    setTimeout("map.AddShape(polyline['Barnstable']);map.SetMapView(polyline['Barnstable'].GetPoints());",14000);
+    setTimeout("map.ShowInfoBox(polyline['Barnstable']);",15000);
+    setTimeout("map.HideInfoBox(polyline['Barnstable']);map.DeleteShape(polyline['Barnstable']);",20000);
+    setTimeout("step6()",21000);
+    
+}
+function step6(){
+
+    //Villager Procedure
+    map.ZoomIn();
+    setTimeout("map.PanToLatLong(new VELatLong(41.654149,-70.300720));",3000);
+    setTimeout("map.ZoomIn();", 4000);
+    setTimeout("map.AddShape(polyline['Villager']);map.SetMapView(polyline['Villager'].GetPoints());",6000);
+    setTimeout("map.ShowInfoBox(polyline['Villager']);",7000);
+    setTimeout("map.HideInfoBox(polyline['Villager']);map.DeleteShape(polyline['Villager']);",12000);
+    
+    //Hyannis Procedure
+    setTimeout("map.PanToLatLong(new VELatLong(41.647006,-70.284777));",13000);
+    setTimeout("map.AddShape(polyline['Hyannis']);map.SetMapView(polyline['Hyannis'].GetPoints());",16000);
+    setTimeout("map.ShowInfoBox(polyline['Hyannis']);",17000);
+    setTimeout("map.HideInfoBox(polyline['Hyannis']);map.DeleteShape(polyline['Hyannis']);",22000);
+    setTimeout("step7()", 23000);
+    
+}
+function step7(){
+
+    map.ZoomIn();
+    setTimeout("map.SetMapStyle('h');",3000);
+    setTimeout("map.PanToLatLong(new VELatLong(41.656730, -70.278693));",6000);
+    setTimeout("map.ZoomIn();",7000);
+    setTimeout("map.ZoomIn();",10000);
+    setTimeout("AddPin( 41.65602625413811,-70.2798843383789, 'Hyannis', 'Hyannis Transportation Center');",11000);
+    setTimeout("map.SetCenterAndZoom(new VELatLong(41.65602625413811,-70.2798843383789), 18);",12000);
+    setTimeout("map.ShowInfoBox(shape)",13000);
+    setTimeout("map.HideInfoBox(shape);map.DeleteShape(shape);",23000);
+    setTimeout("map.ZoomOut();",25000);
+    setTimeout("map.ZoomOut();",27000);
+    setTimeout("map.ZoomOut();",30000);
+    setTimeout("step8()",31000);
+    
+}
+function step8(){
+
+     // **Cleanup and Looping Procedure**
+    setTimeout("map.Clear();map.DeleteAllShapes();",500);
+    setTimeout("setRoutes()",1000);
+    setTimeout("step1()",2000);
+    
+}
