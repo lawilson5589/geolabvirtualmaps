ca947c991253c95b7d258c45b1e5d03e93feedd3
dan1768 <dan1768@6dd3a268-fd34-0410-9aca-e39d7cf66fee>
2007-07-30 16:11:43 +0000

    Added MetroSpeed2 to repository

---
+++ MetroSpeed2/MetroSpeed2/Form1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MetroSpeed2
+{
+    public partial class Metrowest : Form
+    {
+        public Metrowest()
+        {
+            InitializeComponent();
+        }
+    }
+}
